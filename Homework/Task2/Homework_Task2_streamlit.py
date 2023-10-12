b27bae371b3b8f333a680d0d02d9aa5e4388d8db
kieran31415 <r0889344@student.thomasmore.be>
2023-10-12 12:35:38 +0200

    streamlit

--- Homework/Task2/Homework_Task2_streamlit.py
+++ Homework/Task2/Homework_Task2_streamlit.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import category_encoders as ce
 
-student_df = pd.read_csv('./data/student-mat.csv',delimiter=';')
+student_df = pd.read_csv('D:\school23-24\AI\Homework\Task2\data\student-mat.csv',delimiter=';')
 
 independent_vars = ['school', 'sex', 'age', 'address', 'studytime', 'absences', 'G1', 'G2']
 
@@ -28,48 +28,57 @@ st.title('Predict Final Score with Linear regression.')
 new_student_features = {}
 school = st.text_input('What school are you attending (For Gabriel Pereira insert GP, for Mousinho da Silveira insert MS): ').upper().strip()
 sex = st.text_input('What sex are you (For female insert F, for male insert M): ').upper().strip()
-age = st.number_input('How old are you: ')
+age = st.text_input('How old are you: ')
+if age != '':
+    age = int(age)
 address = st.text_input('Do you live in an urban (insert U) or a rural (insert R) neighboorhood: ').upper().strip()
-studytime = st.number_input('How long do you study: ')
-studytime = int(studytime)
-absences = st.number_input('How manny times were you absent: ')
-G1 = st.number_input('What was your first score: ')
-G2 = st.number_input('What was your second score: ')
+studytime = st.text_input('How long do you study: ')
+if studytime != '':
+    studytime = int(studytime)
+absences = st.text_input('How manny times were you absent: ')
+if absences != '':
+    absences = int(absences)
+G1 = st.text_input('What was your first score: ')
+if G1 != '':
+    G1 = int(G1)
+G2 = st.text_input('What was your second score: ')
+if G2 != '':
+    G2 = int(G2)
 
-if school == 'GP':
-    new_student_features['school'] = 1
-elif school == 'MS':
-    new_student_features['school'] = 2
+if school != '' and sex != '' and age != '' and address != '' and studytime != '' and absences != '' and G1 != '' and G2 != '':
+    if school == 'GP':
+        new_student_features['school'] = 1
+    elif school == 'MS':
+        new_student_features['school'] = 2
 
-if sex == 'F':
-    new_student_features['sex'] = 1
-elif sex == 'M':
-    new_student_features['sex'] = 2
+    if sex == 'F':
+        new_student_features['sex'] = 1
+    elif sex == 'M':
+        new_student_features['sex'] = 2
 
-new_student_features['age'] = age
-if address == 'U':
-    new_student_features['address'] = 1
-elif address == 'R':
-    new_student_features['address'] = 2
+    new_student_features['age'] = age
+    if address == 'U':
+        new_student_features['address'] = 1
+    elif address == 'R':
+        new_student_features['address'] = 2
 
-if studytime < 2:
-    new_student_features['studytime'] = 1
-elif studytime < 5:
-    new_student_features['studytime'] = 2
-elif studytime < 10:
-    new_student_features['studytime'] = 3
-else:
-    new_student_features['studytime'] = 4
+    if studytime < 2:
+        new_student_features['studytime'] = 1
+    elif studytime < 5:
+        new_student_features['studytime'] = 2
+    elif studytime < 10:
+        new_student_features['studytime'] = 3
+    else:
+        new_student_features['studytime'] = 4
 
-new_student_features['absences'] = absences
-new_student_features['G1'] = G1
-new_student_features['G2'] = G2
+    new_student_features['absences'] = absences
+    new_student_features['G1'] = G1
+    new_student_features['G2'] = G2
 
-new_student_df = pd.DataFrame([new_student_features])
+    new_student_df = pd.DataFrame([new_student_features])
 
-predicted_G3 = model.predict(new_student_df)
+    predicted_G3 = model.predict(new_student_df).round().astype(int)
+    text = "Predicted final score: "+str(predicted_G3[0])
+    st.text(text)
 
-st.text("Predicted final score:", predicted_G3[0])
-
-#streamlit run Homework_Task2_streamlit.py
-#cd ./AI/Homework/Task2
+#streamlit run ./AI/Homework/Task2/Homework_Task2_streamlit.py
